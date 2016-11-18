5365d3279c274768dae29e7c45c856c440273547
degarashi <slice013@gmail.com>
2016-11-18 12:41:00 +0900

    先行宣言ヘッダを追加

---
+++ fwd.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <cstdint>
+
+namespace lubee {
+	// (fpset.hpp)
+	struct Fp16;
+	struct Fp32;
+
+	// (freelist.hpp)
+	template <class T>
+	class Freelist;
+
+	// (ieee754.hpp)
+	template <class T>
+	struct IEEE754;
+	struct Half;
+
+	// (random.hpp)
+	template <class MT>
+	class Random;
+
+	// (range.hpp)
+	template <class T>
+	struct Range;
+	using RangeI = Range<int_fast32_t>;
+	using RangeF = Range<float>;
+
+	// (size.hpp)
+	template <class T>
+	struct Size;
+	using SizeI = Size<int32_t>;
+	using SizeF = Size<float>;
+
+	// (rect.hpp)
+	template <class T>
+	class Rect;
+	using RectI = Rect<int32_t>;
+	using RectF = Rect<float>;
+	template <class T>
+	struct PP_Lower;
+	template <class T>
+	struct PP_Higher;
+	template <class T, template <class> class Policy>
+	class PowValue;
+	using PowInt = PowValue<uint32_t, PP_Higher>;
+	using PowSize = Size<PowInt>;
+
+	// (wrapper.hpp)
+	template <class T>
+	struct Wrapper;
+}
