c6c41e352e33b4d392044e45f9fdee7515532829
degarashi <slice013@gmail.com>
2016-12-09 17:41:59 +0900

    テンプレートを使用したAnd, Orはlubee::metaへ、そうでないものはlubeeへ定義

--- logical.hpp
+++ logical.hpp
@@ -1,6 +1,38 @@
 #pragma once
 
 namespace lubee {
+	template <class T>
+	inline constexpr T And_A() {
+		return T(0);
+	}
+	template <class T, class... Ts>
+	inline constexpr auto And_A(const T& t0, const Ts&... ts) {
+		return t0 & And_A<T>(ts...);
+	}
+	inline constexpr bool And_L() {
+		return true;
+	}
+	template <class... Ts>
+	inline constexpr bool And_L(const bool b0, const Ts&... ts) {
+		return b0 && And_L(ts...);
+	}
+
+	template <class T>
+	inline constexpr auto Or() {
+		return T(0);
+	}
+	template <class T0, class... Ts>
+	inline constexpr auto Or(const T0& t0, const Ts&... ts) {
+		return t0 | Or(ts...);
+	}
+	inline constexpr auto Or_L() {
+		return false;
+	}
+	template <class... Ts>
+	inline constexpr auto Or_L(const bool b0, const Ts&... ts) {
+		return b0 || Or_L(ts...);
+	}
+
 	//! 引数のペアを全てXORしたものをORする
 	inline constexpr int X_OrArgs() {
 		return 0;
--- meta/boolean.hpp
+++ meta/boolean.hpp
@@ -2,13 +2,15 @@
 #include "constant_t.hpp"
 
 namespace lubee {
-	template <class... Ts>
-	struct And : std::true_type {};
-	template <class T0, class... Ts>
-	struct And<T0, Ts...> : BConst<T0{} && And<Ts...>{}> {};
+	namespace meta {
+		template <class... Ts>
+		struct And : std::true_type {};
+		template <class T0, class... Ts>
+		struct And<T0, Ts...> : BConst<T0{} && And<Ts...>{}> {};
 
-	template <class... Ts>
-	struct Or : std::true_type {};
-	template <class T0, class... Ts>
-	struct Or<T0, Ts...> : BConst<T0{} || Or<Ts...>{}> {};
+		template <class... Ts>
+		struct Or : std::true_type {};
+		template <class T0, class... Ts>
+		struct Or<T0, Ts...> : BConst<T0{} || Or<Ts...>{}> {};
+	}
 }
