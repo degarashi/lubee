decb4d9691bec7658b38b5121ab2b2b41343cf13
degarashi <slice013@gmail.com>
2017-10-16 22:58:41 +0900

    シード値の読み込み

--- tests/test.hpp
+++ tests/test.hpp
@@ -1,14 +1,39 @@
 #pragma once
 #include "../random.hpp"
 #include "../check_serialization.hpp"
+#include "../output.hpp"
+#include <fstream>
+#include <chrono>
 
 namespace lubee {
 	namespace test {
 		class Random : public ::testing::Test {
 			private:
+				using seed_t = RandomMT::result_type;
+				seed_t		_seed;
 				RandomMT	_mt;
 			public:
-				Random(): _mt(RandomMT::Make<4>()) {}
+				Random():
+					_mt(0)
+				{
+					{
+						bool bLoaded = false;
+						// シードファイルがあったら読み込む(とりあえずファイル名は決め打ち)
+						std::ifstream f("/tmp/debug_seed");
+						if(f.is_open()) {
+							f >> _seed;
+							bLoaded = f.good();
+						}
+						if(bLoaded)
+							Log(Verbose, "Loaded Random-seed: %d", _seed);
+						else {
+							_seed = std::chrono::system_clock::now().time_since_epoch().count();
+							Log(Verbose, "Generated Random-seed: %d", _seed);
+						}
+					}
+					RandomMT mt(_seed);
+					std::swap(mt, _mt);
+				}
 				auto& mt() noexcept {
 					return _mt;
 				}
