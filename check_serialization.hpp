910bd6047e82cafdc34dac1156e3003a8fa76771
degarashi <slice013@gmail.com>
2017-10-20 20:52:00 +0900

    CheckSerialization: 関数に比較オブジェクトを指定してもう少し汎用的にした

--- check_serialization.hpp
+++ check_serialization.hpp
@@ -6,12 +6,8 @@
 
 namespace lubee {
 	// ---- for serialization check ----
-	template <class OA, class IA, class Init>
-	void _CheckSerialization(Init&& init) {
-		using T = std::remove_pointer_t<decltype(init())>;
-		using Up = std::unique_ptr<T>;
-		Up ptr0(init()),
-		   ptr1;
+	template <class OA, class IA, class PTR, class CMP>
+	void _CheckSerializationImpl(const PTR& ptr0, PTR& ptr1, const CMP& cmp) {
 		std::stringstream buffer;
 		{
 			OA oa(buffer);
@@ -21,25 +17,33 @@ namespace lubee {
 			IA ia(buffer);
 			ia(cereal::make_nvp("obj", ptr1));
 		}
-		ASSERT_EQ(*ptr0, *ptr1);
+		ASSERT_TRUE(cmp(*ptr0, *ptr1));
 	}
-	template <class Make>
-	void CheckSerializationBin(Make&& m) {
-		_CheckSerialization<cereal::BinaryOutputArchive,
-							cereal::BinaryInputArchive>(m);
+	template <class PTR, class CMP>
+	void _CheckSerializationBin(const PTR& ptr0, PTR& ptr1, const CMP& cmp) {
+		_CheckSerializationImpl<cereal::BinaryOutputArchive,
+								cereal::BinaryInputArchive>(ptr0, ptr1, cmp);
 	}
-	template <class Make>
-	void CheckSerializationJSON(Make&& m) {
-		_CheckSerialization<cereal::JSONOutputArchive,
-							cereal::JSONInputArchive>(m);
+	template <class PTR, class CMP>
+	void _CheckSerializationJSON(const PTR& ptr0, PTR& ptr1, const CMP& cmp) {
+		_CheckSerializationImpl<cereal::JSONOutputArchive,
+								cereal::JSONInputArchive>(ptr0, ptr1, cmp);
 	}
-	template <class Make>
-	void CheckSerializationWithMake(Make&& m) {
-		CheckSerializationBin(m);
-		CheckSerializationJSON(m);
+	template <class PTR, class CMP>
+	void _CheckSerialization(const PTR& ptr0, PTR& ptr1, const CMP& cmp) {
+		_CheckSerializationBin(ptr0, ptr1, cmp);
+		_CheckSerializationJSON(ptr0, ptr1, cmp);
 	}
-	template <class T>
-	void CheckSerialization(const T& src) {
-		CheckSerializationWithMake([&](){ return new T(src); });
+	template <class T, class CMP = std::equal_to<>>
+	void CheckSerialization(const T& src, const CMP& cmp = CMP()) {
+		using Up = std::unique_ptr<T>;
+		Up ptr0(new T(src)),
+		   ptr1;
+		_CheckSerialization(ptr0, ptr1, cmp);
+	}
+	template <class T, class CMP = std::equal_to<>>
+	void CheckSerialization(const std::shared_ptr<T>& sp, const CMP& cmp = CMP()) {
+		std::shared_ptr<T> sp1;
+		_CheckSerialization(sp, sp1, cmp);
 	}
 }
