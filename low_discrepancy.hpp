9b224a8a87e6027ecb5a10d6baa6aa7e1aa56a29
degarashi <slice013@gmail.com>
2018-09-25 16:09:18 +0900

    fix "multiple definition"

--- low_discrepancy.hpp
+++ low_discrepancy.hpp
@@ -5,7 +5,7 @@
 #include <cstdint>
 
 namespace lubee {
-	double VanDerCorput(uint32_t i, uint32_t base) {
+	inline double VanDerCorput(uint32_t i, uint32_t base) {
 		double h = 0,
 			   f = 1.0 / base,
 			   factor = f;
@@ -21,18 +21,18 @@ namespace lubee {
 		const uint32_t c_primes[] = {2, 3, 5, 7, 9, 11, 13, 17, 19, 23};
 		constexpr std::size_t N_Primes = countof(c_primes);
 
-		void CheckDim(const std::size_t dim) {
+		inline void CheckDim(const std::size_t dim) {
 			D_Assert(dim <= N_Primes, "too big dimension");
 		}
 	}
-	void Halton(double* dst, const uint32_t i, const std::size_t dim) {
+	inline void Halton(double* dst, const uint32_t i, const std::size_t dim) {
 		detail::CheckDim(dim);
 
 		for(std::size_t j=0; j<dim ; j++)
 			*dst++ = VanDerCorput(i, detail::c_primes[j]);
 	}
 	using Double_V = std::vector<double>;
-	Double_V Hammersley(const std::size_t n, const std::size_t dim) {
+	inline Double_V Hammersley(const std::size_t n, const std::size_t dim) {
 		detail::CheckDim(dim);
 
 		Double_V res(n*dim);
